80969c22bf762f9ef88739d63dd55892852d1416
Francis Acquaah <francisk994@gmail.com>
2020-08-11 02:16:34 +0000

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by KOFI on 8/11/20.
+//  Copyright © 2020 fiifi_gh. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
